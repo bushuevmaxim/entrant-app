ebf3cb5042e639d1ae27dde1b025f359e42cd370
Max Bushuev <bushuevmaxim55@gmail.com>
2025-01-08 16:21:48 +0600

    Add Course card

--- lib/src/features/home/data/repositories/courses_repository.dart
+++ lib/src/features/home/data/repositories/courses_repository.dart
@@ -5,10 +5,10 @@ class MockCoursesRepository implements ICoursesRepository {
   static final List<Course> _courses = [
     const Course(
       id: '0',
-      name: "Computer Science",
-      code: "CS101",
+      name: "Фундаментальная информатика и информационные технологии",
+      code: "02.03.02",
       profile: "Bachelor of Science in Computer Science",
-      formEducation: "Full-time",
+      formEducation: "Бакалавриат",
       countPlaces: 50,
       passingScore: 75,
       graduates: [
@@ -30,10 +30,11 @@ class MockCoursesRepository implements ICoursesRepository {
     ),
     const Course(
       id: '1',
-      name: "Business Administration",
-      code: "BA202",
+      name:
+          "Математическое обеспечение и администрирование информационных систем",
+      code: "02.03.03",
       profile: "Bachelor of Business Administration",
-      formEducation: "Part-time",
+      formEducation: "Бакалавриат",
       countPlaces: 30,
       passingScore: 70,
       graduates: [
@@ -51,55 +52,6 @@ class MockCoursesRepository implements ICoursesRepository {
       requiredSubjects: ["Economics", "Accounting", "Management"],
       optionalSubjects: ["Entrepreneurship", "Business Law"],
       curriculumUrl: "https://example.com/ba_curriculum",
-    ),
-    const Course(
-      id: '2',
-      name: "Graphic Design",
-      code: "GD303",
-      profile: "Bachelor of Arts in Graphic Design",
-      formEducation: "Full-time",
-      countPlaces: 25,
-      passingScore: 80,
-      graduates: [
-        Graduate(
-          name: "Emma Brown",
-          date: "2024-01-20",
-          company: Company(name: "Creative Agency"),
-          position: "Graphic Designer",
-          imageUrl: "https://example.com/emma.jpg",
-        ),
-      ],
-      practiceCompanies: [
-        Company(name: "Creative Agency"),
-        Company(name: "Design Studio"),
-      ],
-      requiredSubjects: ["Design Principles", "Color Theory"],
-      optionalSubjects: ["Photography", "3D Modeling"],
-      curriculumUrl: "https://example.com/gd_curriculum",
-    ),
-    const Course(
-      id: '3',
-      name: "Mechanical Engineering",
-      code: "ME404",
-      profile: "Bachelor of Science in Mechanical Engineering",
-      formEducation: "Full-time",
-      countPlaces: 40,
-      passingScore: 85,
-      graduates: [
-        Graduate(
-          name: "Michael Johnson",
-          date: "2023-08-30",
-          company: Company(name: "Engineering Solutions"),
-          position: "Mechanical Engineer",
-        ),
-      ],
-      practiceCompanies: [
-        Company(name: "Engineering Solutions"),
-        Company(name: "Innovative Designs"),
-      ],
-      requiredSubjects: ["Thermodynamics", "Fluid Mechanics"],
-      optionalSubjects: ["Robotics", "Materials Science"],
-      curriculumUrl: "https://example.com/me_curriculum",
     ),
   ];
   @override
