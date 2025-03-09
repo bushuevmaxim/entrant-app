986cb1ca579db9f48f40811f98f666611e39c7dd
Max Bushuev <bushuevmaxim55@gmail.com>
2025-03-09 13:00:23 +0600

    Add profile section title

--- lib/src/features/detailed_programm/presentation/detailed_programm_page.dart
+++ lib/src/features/detailed_programm/presentation/detailed_programm_page.dart
@@ -88,6 +88,14 @@ class ProgrammContent extends StatelessWidget {
             ),
           ),
         ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: InfoSectionTitle(
+              title: 'Профили',
+            ),
+          ),
+        ),
         ProfileSection(
           profiles: programm.profiles,
         ),
