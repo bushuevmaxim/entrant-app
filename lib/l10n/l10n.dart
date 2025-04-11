2b90d48d2a902788f63d02277f958602c0977daf
Max Bushuev <bushuevmaxim55@gmail.com>
2025-04-11 10:31:29 +0600

    Fix formatting

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 class L10n {
-  static const all = [
-    Locale('en'),
-    Locale('ru'),
-  ];
+  static const all = [Locale('en'), Locale('ru')];
 
   static const defaultLocale = Locale('ru');
 }
