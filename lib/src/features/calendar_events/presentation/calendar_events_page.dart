e46da14c951263f315ea50c01869586b5d0dfa07
Max Bushuev <bushuevmaxim55@gmail.com>
2025-04-13 11:54:30 +0600

    Remove AppBar from CalendarEventsPage

--- lib/src/features/calendar_events/presentation/calendar_events_page.dart
+++ lib/src/features/calendar_events/presentation/calendar_events_page.dart
@@ -48,87 +48,94 @@ class _CalendarEventsViewState extends State<CalendarEventsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(context.l10n.calendarEvents)),
-      body: Column(
-        children: [
-          BlocBuilder<CalendarEventsBloc, CalendarEventsState>(
-            builder: (context, state) {
-              return ValueListenableBuilder<DateTime>(
-                valueListenable: _selectedDayNotifier,
-                builder: (context, focusedDay, _) {
-                  return TableCalendar<Event>(
-                    firstDay: kToday,
-                    lastDay: _lastDay,
-                    focusedDay: focusedDay,
-                    selectedDayPredicate: (day) => isSameDay(day, focusedDay),
-                    calendarStyle: const CalendarStyle(
-                      outsideDaysVisible: false,
-                    ),
-                    headerStyle: const HeaderStyle(formatButtonVisible: false),
-                    eventLoader: (day) {
-                      final date = DateTime(day.year, day.month, day.day);
-                      final events = state.events[date] ?? [];
-                      return events;
-                    },
-                    onPageChanged: (focusedDay) {
-                      _selectedDayNotifier.value = focusedDay;
-                      context.read<CalendarEventsBloc>().add(
-                        CalendarEventsEvent.loadEvents(
-                          selectedMonth: focusedDay,
-                        ),
-                      );
-                    },
-                    onDaySelected: (selectedDay, focusedDay) {
-                      _selectedDayNotifier.value = selectedDay;
-                    },
-                    locale: Localizations.localeOf(context).toString(),
-                  );
-                },
-              );
-            },
-          ),
-          const SizedBox(height: 8.0),
-          Expanded(
-            child: BlocBuilder<CalendarEventsBloc, CalendarEventsState>(
+      body: SafeArea(
+        child: Column(
+          children: [
+            BlocBuilder<CalendarEventsBloc, CalendarEventsState>(
               builder: (context, state) {
-                return switch (state) {
-                  CalendarEventsState(isLoading: true) => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                  CalendarEventsState(error: final error?) => Center(
-                    child: Text(context.l10n.somethingWentWrong),
-                  ),
-                  CalendarEventsState(events: final events) =>
-                    ValueListenableBuilder(
-                      valueListenable: _selectedDayNotifier,
-                      builder: (context, selectedDay, child) {
-                        final date = DateTime(
-                          selectedDay.year,
-                          selectedDay.month,
-                          selectedDay.day,
-                        );
-                        final dayEvents = events[date] ?? [];
-
-                        if (dayEvents.isEmpty) {
-                          return Center(child: Text(context.l10n.emptyEvents));
-                        }
-
-                        return ListView.separated(
-                          padding: const EdgeInsets.all(16),
-                          itemCount: dayEvents.length,
-                          separatorBuilder:
-                              (context, index) => const SizedBox(height: 8),
-                          itemBuilder: (context, index) {
-                            return EventCard(event: dayEvents.elementAt(index));
-                          },
+                return ValueListenableBuilder<DateTime>(
+                  valueListenable: _selectedDayNotifier,
+                  builder: (context, focusedDay, _) {
+                    return TableCalendar<Event>(
+                      firstDay: kToday,
+                      lastDay: _lastDay,
+                      focusedDay: focusedDay,
+                      selectedDayPredicate: (day) => isSameDay(day, focusedDay),
+                      calendarStyle: const CalendarStyle(
+                        outsideDaysVisible: false,
+                      ),
+                      headerStyle: const HeaderStyle(
+                        formatButtonVisible: false,
+                      ),
+                      eventLoader: (day) {
+                        final date = DateTime(day.year, day.month, day.day);
+                        final events = state.events[date] ?? [];
+                        return events;
+                      },
+                      onPageChanged: (focusedDay) {
+                        _selectedDayNotifier.value = focusedDay;
+                        context.read<CalendarEventsBloc>().add(
+                          CalendarEventsEvent.loadEvents(
+                            selectedMonth: focusedDay,
+                          ),
                         );
                       },
-                    ),
-                };
+                      onDaySelected: (selectedDay, focusedDay) {
+                        _selectedDayNotifier.value = selectedDay;
+                      },
+                      locale: Localizations.localeOf(context).toString(),
+                    );
+                  },
+                );
               },
             ),
-          ),
-        ],
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: BlocBuilder<CalendarEventsBloc, CalendarEventsState>(
+                builder: (context, state) {
+                  return switch (state) {
+                    CalendarEventsState(isLoading: true) => const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    CalendarEventsState(error: final error?) => Center(
+                      child: Text(context.l10n.somethingWentWrong),
+                    ),
+                    CalendarEventsState(events: final events) =>
+                      ValueListenableBuilder(
+                        valueListenable: _selectedDayNotifier,
+                        builder: (context, selectedDay, child) {
+                          final date = DateTime(
+                            selectedDay.year,
+                            selectedDay.month,
+                            selectedDay.day,
+                          );
+                          final dayEvents = events[date] ?? [];
+
+                          if (dayEvents.isEmpty) {
+                            return Center(
+                              child: Text(context.l10n.emptyEvents),
+                            );
+                          }
+
+                          return ListView.separated(
+                            padding: const EdgeInsets.all(16),
+                            itemCount: dayEvents.length,
+                            separatorBuilder:
+                                (context, index) => const SizedBox(height: 8),
+                            itemBuilder: (context, index) {
+                              return EventCard(
+                                event: dayEvents.elementAt(index),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                  };
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
